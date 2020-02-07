b489f96fb40b75925c5bc97174a10dd7703e1966
offiahchiaz <offiahchiaz@gmail.com>
2020-02-07 04:41:47 +0100

    Updated profile.show blade to display user description

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -28,7 +28,7 @@
             </div>
             <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
             <div>
-                {{-- {{ $user->profile->description }} --}}
+                {{ $user->profile->description }}
                 {{-- We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit. --}}
             </div>
         </div>
